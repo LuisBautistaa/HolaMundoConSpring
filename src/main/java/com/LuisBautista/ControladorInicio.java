b1a69f382c69ec895921c38da614ae6e3a85ae85
Luis <lhbr98@gmail.com>
2021-06-16 23:17:40 -0500

    Se agrego configuracion para compartir informacion con thymeleaf y spring MVC

--- src/main/java/com/LuisBautista/ControladorInicio.java
+++ src/main/java/com/LuisBautista/ControladorInicio.java
@@ -1,23 +1,34 @@
 package com.LuisBautista;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 /**
  *
  * @author Luis Bautista
  * Clase que contiene un controlador de tipo rest, para desplegar inffo alnavegador
  * asi como el servidor enviando informacion con el log.
  */
-@RestController
+@Controller
 @Slf4j //para tener acceso a la variable log
 public class ControladorInicio {
     
+    //para acceder valores dentro del application.properties hacemos lo sig.
+    @Value("${index.saludo}")
+    private String saludo;//aca se inyecta el valor de los properties
+    
+    //para procesar la informacion de tipo get e indicar el path para la ruta de inicio 
     @GetMapping("/")      
-    public String inicio(){
-        log.info("Ejecutando el Controlador rest");
-        log.debug("Mas detalles del Controladorr");
-        return "Hola Mundoo con SPRING reload!! 2";
+    public String inicio(Model model){  
+        //para enviar info a la vista se hace uso de model para enviar el atributo
+        var mensaje = "Enviando info a la vista con Model y Thymeleaf";
+        log.info("Ejecutando el Controlador Spring MVC"); 
+        //se agrega el atributo a model de clave valor para que lo pueda usar la vista
+        model.addAttribute("mensaje", mensaje);
+        model.addAttribute("saludo", saludo);
+        return "index";
     }
 
 }
